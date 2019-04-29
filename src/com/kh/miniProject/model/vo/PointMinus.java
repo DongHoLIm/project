f5c80bdce5758f8d18125e7c776b3adfee14e71d
eunju <eunju@DESKTOP-IE2PDJ4>
2019-04-29 19:29:43 +0900

    yasssssss

--- src/com/kh/miniProject/model/vo/PointMinus.java
+++ src/com/kh/miniProject/model/vo/PointMinus.java
@@ -1,5 +1,20 @@
 package com.kh.miniProject.model.vo;
 
 public class PointMinus {
+	public static int resultcoin =5000;
+	   public void pointMinus(int minus) {   
+		  if (resultcoin>=minus) {
+			  resultcoin = resultcoin-minus;  
+			  System.out.println(resultcoin);
+		}else {
+			System.out.println("µ∑ ∫Œ¡∑");
+		}
+
+	   }
+	   public void pointPlus(int plus) {
+		   resultcoin = resultcoin+plus;
+	      
+	   }	
+	
 
 }
